c62d93165bb8e3df23cc258d395e7dbf01888362
Mayor Creator <paulaboagye@outlook.com>
2025-06-06 21:39:00 -0400

    refactor ProductList to use picture element for responsive images

--- src/component/productList/ProductList.tsx
+++ src/component/productList/ProductList.tsx
@@ -9,7 +9,19 @@ export const ProductList = ({ data }: ProductListProps) => {
     <div className="product-list">
       {data.map((product) => (
         <div key={product.name} className="product-item">
-          <img src={product.image.desktop} alt={product.category} />
+          <picture>
+            {/* Desktop: 1024px and above */}
+            <source
+              media="(min-width: 1024px)"
+              srcSet={product.image.desktop}
+            />
+            {/* Tablet: 768px to 1023px */}
+            <source media="(min-width: 768px)" srcSet={product.image.tablet} />
+            {/* Mobile: below 768px */}
+            <source srcSet={product.image.mobile} />
+            {/* Fallback image if picture element is not supported */}
+            <img src={product.image.mobile} alt={product.name} loading="lazy" />
+          </picture>
           <p>{product.category}</p>
           <p>{product.name}</p>
           <p>${product.price.toFixed(2)}</p>
