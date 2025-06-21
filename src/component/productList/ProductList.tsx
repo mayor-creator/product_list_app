d8c853a2a5a995569643689bf9de85a15f0fa350
Mayor Creator <paulaboagye@outlook.com>
2025-06-21 17:27:01 -0400

    style: enhance responsive design for main wrapper and product list components

--- src/component/productList/ProductList.tsx
+++ src/component/productList/ProductList.tsx
@@ -22,51 +22,68 @@ export const ProductList = ({
   onUpdateQuantity,
 }: ProductListProps) => {
   return (
-    <section className="product-list">
-      {data.map((product) => (
-        <article key={product.name} className="product-item">
-          <div className={styles.productImageContainer}>
-            <picture>
-              <source
-                media="(min-width: 1024px)"
-                srcSet={product.image.desktop}
-              />
-              <source
-                media="(min-width: 768px)"
-                srcSet={product.image.tablet}
-              />
-              <source srcSet={product.image.mobile} />
-              <img
-                className={styles.productImageItem}
-                src={product.image.mobile}
-                alt={product.name}
-                loading="lazy"
-              />
-            </picture>
-            <div className={styles.productButtonOverlay}>
-              {cartItems[product.name] ? (
-                <CartItemControlButton
-                  quantity={cartItems[product.name]}
-                  onDecrease={() =>
-                    onUpdateQuantity(product.name, cartItems[product.name] - 1)
-                  }
-                  onIncrease={() =>
-                    onUpdateQuantity(product.name, cartItems[product.name] + 1)
-                  }
-                />
-              ) : (
-                <AddToCartButton onClick={() => onAddToCart(product.name)} />
-              )}
-            </div>
-          </div>
+    <>
+      <div className="productListContainer">
+        <section>
+          <h1>Desserts</h1>
+        </section>
+        <section className={styles.productList}>
+          {data.map((product) => (
+            <article key={product.name} className="product-item">
+              <div className={styles.productImageContainer}>
+                <picture>
+                  <source
+                    media="(min-width: 1024px)"
+                    srcSet={product.image.desktop}
+                  />
+                  <source
+                    media="(min-width: 768px)"
+                    srcSet={product.image.tablet}
+                  />
+                  <source srcSet={product.image.mobile} />
+                  <img
+                    className={styles.productImageItem}
+                    src={product.image.mobile}
+                    alt={product.name}
+                    loading="lazy"
+                  />
+                </picture>
+                <div className={styles.productButtonOverlay}>
+                  {cartItems[product.name] ? (
+                    <CartItemControlButton
+                      quantity={cartItems[product.name]}
+                      onDecrease={() =>
+                        onUpdateQuantity(
+                          product.name,
+                          cartItems[product.name] - 1
+                        )
+                      }
+                      onIncrease={() =>
+                        onUpdateQuantity(
+                          product.name,
+                          cartItems[product.name] + 1
+                        )
+                      }
+                    />
+                  ) : (
+                    <AddToCartButton
+                      onClick={() => onAddToCart(product.name)}
+                    />
+                  )}
+                </div>
+              </div>
 
-          <div className="product-details">
-            <p className={styles.productCategory}>{product.category}</p>
-            <p className={styles.productName}>{product.name}</p>
-            <p className={styles.productPrice}>${product.price.toFixed(2)}</p>
-          </div>
-        </article>
-      ))}
-    </section>
+              <div className="product-details">
+                <p className={styles.productCategory}>{product.category}</p>
+                <p className={styles.productName}>{product.name}</p>
+                <p className={styles.productPrice}>
+                  ${product.price.toFixed(2)}
+                </p>
+              </div>
+            </article>
+          ))}
+        </section>
+      </div>
+    </>
   );
 };
