0829647c7f92c7bccfeafad8d186edc626a281ac
Mayor Creator <paulaboagye@outlook.com>
2025-07-16 22:19:35 -0400

    style: enhance ConfirmOrderModal with improved layout and styling

--- src/component/modal/OrderModal.tsx
+++ src/component/modal/OrderModal.tsx
@@ -1,5 +1,6 @@
 import orderConfirmedIcon from "../../assets/images/icon-order-confirmed.svg";
 import { Button } from "../ui/button/Button";
+import styles from "./OrderModal.module.css";
 
 interface CartItem {
 	name: string;
@@ -31,38 +32,56 @@ export const ConfirmOrderModal = ({
 	const total = calculateTotal(items);
 
 	return (
-		<>
-			<img src={orderConfirmedIcon} alt="check mark icon" />
-			<div>
-				<button type="button" onClick={onClose}>
-					X
-				</button>
+		<div className={styles.modalOverlay}>
+			<div className={styles.titleInfoContainer}>
+				<div className={styles.modalInfoContainer}>
+					<img
+						src={orderConfirmedIcon}
+						alt="check mark icon"
+						className={styles.modalImage}
+					/>
+					<button type="button" onClick={onClose}>
+						X
+					</button>
+				</div>
+				<div>
+					<h1 className={styles.modalTitle}>Order Confirmed</h1>
+					<p className={styles.modalMessage}>We hope you enjoy your food!</p>
+				</div>
 			</div>
-			<h1>Order Confirmed</h1>
-			<p>We hope you enjoy your food!</p>
 
-			<div>
+			<div className={styles.modalItemContainer}>
 				{items.length > 0
 					? items.map((item) => (
-							<div key={item.name}>
-								<p>{item.name}</p>
-								<div>
-									<span>{item.quantity}x</span>
-									<span>${item.price.toFixed(2)}</span>
-									<span>${(item.quantity * item.price).toFixed(2)}</span>
+							<div key={item.name} className={styles.modalItem}>
+								<p className={styles.modalItemName}>{item.name}</p>
+								<div className={styles.modalItemChild}>
+									<span className={styles.modalItemQuantity}>
+										{item.quantity}x
+									</span>
+									<span className={styles.modalItemPrice}>
+										${item.price.toFixed(2)}
+									</span>
+									<span className={styles.modalItemTotal}>
+										${(item.quantity * item.price).toFixed(2)}
+									</span>
 								</div>
-								<hr />
+								<hr className={styles.modalDivider} />
 							</div>
 						))
 					: null}
+
+				<div className={styles.modalTotal}>
+					<span className={styles.orderTotalTitle}>Order Total</span>
+					<span className={styles.orderTotalPrice}>${total.toFixed(2)}</span>
+				</div>
 			</div>
-			<div>
-				<span>Order Total</span>
-				<span>${total.toFixed(2)}</span>
+
+			<div className={styles.modalButtonContainer}>
+				<Button variant="tertiary" size="medium" onClick={onStartNewOrder}>
+					Start New Order
+				</Button>
 			</div>
-			<Button variant="primary" size="medium" onClick={onStartNewOrder}>
-				Start New Order
-			</Button>
-		</>
+		</div>
 	);
 };
